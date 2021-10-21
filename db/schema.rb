c7190e158777a05d4ce7ac7325b0fc7a68932131
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-10-20 21:33:39 -0400

    adds location column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_011909) do
+ActiveRecord::Schema.define(version: 2021_10_21_013253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_10_21_011909) do
     t.boolean "special_needs"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "location"
   end
 
 end
