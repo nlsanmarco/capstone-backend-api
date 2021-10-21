fb13c85b0007be9b76349b9c3a791df30d8daec5
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-10-20 22:12:55 -0400

    adds authorizations and user show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user except: [:create]
+  before_action :authenticate_user, except: [:create]
+
   def create
     user = User.new(
       name: params[:name],
@@ -15,6 +16,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    
+    user = User.find(current_user.id)
+    render json: user.as_json
   end
 end
