3c202af2347a0b156bf4ef09f5dbc279bad6f5de
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-10-26 10:47:47 -0400

    adds attributes to users serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :email, :has_dogs, :has_cats, :has_children, :lives_in_house, :has_yard, :dog_training_experience, :hours_away_per_day, :preferred_breed, :preferred_age, :preferred_gender, :preferred_size, :sepcial_needs, :location
 end
