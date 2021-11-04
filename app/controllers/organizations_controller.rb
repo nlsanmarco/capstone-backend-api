d2586637cfdf94cf62531c8b3ddeeab772fec8ce
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-11-04 14:52:33 -0400

    updates organizations show action to parse out 'organization'

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,7 +2,7 @@ class OrganizationsController < ApplicationController
   before_action :authenticate_user
 
   def show
-    organization = HTTP.auth("Bearer #{get_token}").get("https://api.petfinder.com/v2/organizations/#{params[:org_id]}").parse(:json)
+    organization = HTTP.auth("Bearer #{get_token}").get("https://api.petfinder.com/v2/organizations/#{params[:org_id]}").parse(:json)["organization"]
 
     render json: organization
   end
