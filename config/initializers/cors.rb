9f73faa2e9dff55aba871f0d8c913ef4e8d6256a
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-11-15 13:55:19 -0500

    updates cors file:

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "localhost:8080",
-            "https://tn-dog-backend.herokuapp.com/", "tndog.netlify.app"
+            "tndog.netlify.app"
 
     resource "*",
       headers: :any,
