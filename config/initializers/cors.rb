3d6abb426a1c23b3cbb6868d1f42ce6432c96e33
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-11-15 13:28:54 -0500

    adds netlify site to cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "example.com"
+    origins "localhost:8080",
+            "https://tn-dog-backend.herokuapp.com/", "tndog.netlify.app"
 
     resource "*",
       headers: :any,
